f852460f63619bc8a145355f2b45623bca3391fa
JeanPaul CE <jpcdlalito@hotmail.com>
2023-05-29 01:30:17 +0000

    correcting errors

--- project/setup.sh
+++ project/setup.sh
@@ -2,6 +2,6 @@
 
 /usr/local/bin/gitpod-setup-ddev.sh
 ddev composer install
-drush cset system.site uuid "69d5cd67-c5c1-4b29-ac4a-7ddd87944beb"
+ddev drush cset system.site uuid "69d5cd67-c5c1-4b29-ac4a-7ddd87944beb"
 ddev exec drush cim
 
