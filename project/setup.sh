8a0fc8e193e2a58e2e56071e09b12cc9673a3417
JeanPaul CE <jpcdlalito@hotmail.com>
2023-05-29 01:38:53 +0000

    config-import

--- project/setup.sh
+++ project/setup.sh
@@ -3,5 +3,7 @@
 /usr/local/bin/gitpod-setup-ddev.sh
 ddev composer install
 ddev drush cset system.site uuid "69d5cd67-c5c1-4b29-ac4a-7ddd87944beb"
-ddev exec drush cim
+ddev drush config:import
+ddev drush config-import
+ddev drush cim
 
